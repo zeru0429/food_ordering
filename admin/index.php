b59877934ad48c2245c0e536dc8bd38f5aff0f88
zeru0429 <mihiretutigistu@gmail.com>
2023-06-11 23:15:55 +0300

    on progress

---
+++ admin/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin dash bord</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!----------  Header section ----------->
+
+    <div class="menu">
+    <div class="wrapper">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Admin</a></li>
+            <li><a href="#">Catagory</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Order</a></li>
+        </ul>
+    </div>
+
+    </div>
+
+    <!----X-----  Header section -----X----->
+
+
+
+
+    <!----------  Main section ----------->
+    <div class="main">
+    <div class="wrapper">
+    <h1>Dashbord</h1 >
+
+    <div class="catagor text-center">
+        <h2>5</h2>
+        <br>
+        Catagory
+    </div>
+    
+    <div class="catagor text-center">
+        <h2>5</h2>
+        <br>
+        Catagory
+    </div>
+
+    <div class="catagor text-center">
+        <h2>5</h2>
+        <br>
+        Catagory
+    </div>
+
+    <div class="catagor text-center">
+        <h2>5</h2>
+        <br>
+        Catagory
+    </div>
+
+<div class="clear-fixt"></div>
+    </div>
+    
+    </div>
+
+
+    <!----X-----  Main section -----X----->
+
+    
+
+
+    <!----------  Footer section ----------->
+
+    <div class="footer">
+    <div class="wrapper">
+        <p class="text-center">food ordering app 2023 developed by <a href="#"> zeru0429</a> </p>
+    </div>
+    
+    </div>
+
+    <!----X-----  Footer section -----X----->
+
+
+</body>
+
+
+</html>
